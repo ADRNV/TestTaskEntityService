a6745327674f03a3b0aec127993cb29e0b5dca4e
ADRN_NKT <combinerk@gmail.com>
2023-10-07 00:45:46 +0300

    Add models for domain

---
+++ EntityService.Core/BankProp.cs
@@ -0,0 +1,15 @@
+﻿namespace EntityService.Core
+{
+    public class BankProp
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int BIC { get; set; }
+
+        public string SettlementCheck { get; set; }
+
+        public string CorrespondentCheck { get; set; }
+    }
+}
---
+++ EntityService.Core/EntityFace.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityService.Core
+{
+    public class EntityFace
+    {
+        public Guid Id { get; set; }
+        public string ActivityType { get; set; }
+
+        public string TIN { get; set; } 
+
+        public string MSRN { get; set; }
+    }
+}
