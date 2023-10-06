84a1b13d268fad1a9b1451301c83b7a9a4c2846d
ADRN_NKT <combinerk@gmail.com>
2023-10-07 00:17:04 +0300

    Добавьте файлы проекта.

---
+++ EntityService.Infrastructure/Entityes/EntityFace.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityService.Infrastructure.Entityes
+{
+    internal class EntityFace
+    {
+    }
+}
